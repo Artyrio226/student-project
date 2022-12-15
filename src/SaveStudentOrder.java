bf90720d174afabec295d63b9a36c45a8a181530
Artyrio226 <artur_sar_80@mail.ru>
2022-12-15 12:57:10 +0300

    Результат после 9-го урока!!!

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -2,15 +2,13 @@ public class SaveStudentOrder {
 
     public static void main(String[] args) {
 
-        StudentOrder so;
-        so = new StudentOrder();
+        StudentOrder so = new StudentOrder();
         so.hFirstName = "Алексей";
         so.hLastName = "Петров";
         so.wFirstName = "Галина";
         so.wLastName = "Петрова";
 
-        StudentOrder so1;
-        so1 = new StudentOrder();
+        StudentOrder so1 = new StudentOrder();
         so1.hFirstName = "Алексей";
         so1.hLastName = "Сидоров";
         so1.wFirstName = "Галина";
