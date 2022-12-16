ce62a104924e2c7700bb784571ccdf3e985e889a
Artyrio226 <artur_sar_80@mail.ru>
2022-12-16 17:14:21 +0300

    Результат после 17-го урока!!!

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -7,7 +7,6 @@ public class SaveStudentOrder {
 
     public static void main(String[] args) {
 
-        buildStudentOrder();
 
 //        StudentOrder so = new StudentOrder();
 //        long ans = saveStudentOrder(so);
@@ -23,20 +22,11 @@ public class SaveStudentOrder {
 
     }
 
-    static StudentOrder buildStudentOrder() {
+    public static StudentOrder buildStudentOrder(long id) {
         StudentOrder so = new StudentOrder();
+        so.setStudentOrderId(id);
 
 
-        Adult husband = new Adult();
-
-
-//        husband.setGivenName("Андрей");
-//        husband.setSurName("Петров");
-//        husband.setPassportNumber("123456");
-//        so.setHusband(husband);
-//
-//        String ans = husband.getPersonString();
-//        System.out.println(ans);
 
         return so;
     }
--- src/edu/javacourse/studentorder/StudentOrderValidator.java
+++ src/edu/javacourse/studentorder/StudentOrderValidator.java
@@ -30,31 +30,39 @@ public class StudentOrderValidator {
 
     public void checkAll() {
 
-        while (true) {
-            StudentOrder so = readStudentOrder();
-            if (so == null) {
-                break;
-            }
+            StudentOrder[] soArray = readStudentOrders();
 
-            AnswerCityRegister cityAnswer = checkCityRegister(so);
-            if (!cityAnswer.success) {
-                //
-               // continue;
-                break;
-            }
+//            for (int c = 0; c < soArray.length; c++) {
+//                System.out.println();
+//                checkOneOrder(soArray[c]);
+//            }
 
-            AnswerWedding wedAnswer = checkWedding(so);
-            AnswerChildren childAnswer = checkChildren(so);
-            AnswerStudent studentAnswer = checkStudent(so);
-
-            sendMail(so);
+        for (StudentOrder so: soArray) {
+            System.out.println();
+            checkOneOrder(so);
         }
+
     }
 
-    public StudentOrder readStudentOrder() {
-        StudentOrder so = new StudentOrder();
-        return so;
+    public StudentOrder[] readStudentOrders() {
+        StudentOrder[] soArray = new StudentOrder[5];
+
+        for (int c = 0; c < soArray.length; c++) {
+            soArray[c] = SaveStudentOrder.buildStudentOrder(c);
+        }
+
+        return soArray;
     }
+
+    public void checkOneOrder(StudentOrder so) {
+        AnswerCityRegister cityAnswer = checkCityRegister(so);
+        AnswerWedding wedAnswer = checkWedding(so);
+        AnswerChildren childAnswer = checkChildren(so);
+        AnswerStudent studentAnswer = checkStudent(so);
+
+        sendMail(so);
+    }
+
 
     public AnswerCityRegister checkCityRegister(StudentOrder so) {
         return cityRegisterVal.checkCityRegister(so);
