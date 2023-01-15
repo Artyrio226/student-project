ae7835c2a23a46b3bb31ff08bb02e309ad238fbe
Artyrio226 <artur_sar_80@mail.ru>
2023-01-15 11:27:58 +0300

    Результат после 50-го урока!!!

--- src/edu/javacourse/studentorder/dao/StudentDaoImpl.java
+++ src/edu/javacourse/studentorder/dao/StudentDaoImpl.java
@@ -87,8 +87,9 @@ public class StudentDaoImpl implements StudentOrderDao
             for (Child child : so.getChildren()) {
                 stmt.setLong(1, soId);
                 setParamsForChild(stmt, child);
-                stmt.executeUpdate();
+                stmt.addBatch();
             }
+            stmt.executeBatch();
         }
     }
 
